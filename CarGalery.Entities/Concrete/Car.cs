51f87c4d84993476b608219e95009870da78e2c2
sumdeg06@gmail.com <sumdeg06@gmail.com>
2021-01-07 13:15:08 +0300

    Entities

---
+++ CarGalery.Entities/Concrete/Car.cs
@@ -0,0 +1,30 @@
+﻿using CarGalery.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarGalery.Entities.Concrete
+{
+   public class Car :IEntity
+    {
+        public int CarId { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+
+        public Car()
+        {
+
+        }
+
+        public Car(string model, decimal price)
+        {
+            Model = model;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+           return $"{CarId,-5} {Model,-30} {Price,-10}";
+        }
+    }
+}
